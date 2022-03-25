c4c163522a3d2d591e53616e703fe182d682c5a0
Anri <anri0806@hotmail.co.jp>
2022-03-25 17:25:50 -0400

    commit tree 59e0ac2392618570ec04d11f06a34e0156cfbe36

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,23 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        #  This does find if dev received given 
+        #  item but doesnt return boolean
+        #self.freebies.find_by(item_name: item_name)
+
+        self.freebies.any? do |r|
+            r.item_name == item_name
+        end
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev != self
+        freebie.dev = dev
+        end
+
+        #freebie.update(dev: dev) unless freebie.dev != self
+    end
 
 end
